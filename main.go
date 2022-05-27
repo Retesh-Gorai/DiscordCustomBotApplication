9d7fc83a63e7ba807dd702dc3e64dcd8101fcd2e
Retesh-Gorai <retesh.retesh.gorai@gmail.com>
2022-05-27 18:03:40 +0530

    initial commit for bot app

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Retesh-Gorai/DiscordCustomBotApplication/bot"
+	"github.com/Retesh-Gorai/DiscordCustomBotApplication/config"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
